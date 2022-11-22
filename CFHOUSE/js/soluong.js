37647945a438b359fa67a986c0ba9bd14f0e3856
NTDyn <114789770+NTDyn@users.noreply.github.com>
2022-11-22 14:18:28 +0700

    update

--- CFHOUSE/js/soluong.js
+++ CFHOUSE/js/soluong.js
@@ -21,3 +21,44 @@ $(".plus-minus-qty").click(function(){
         minus(el);
 
 })
+
+$(document).on('click','.btn-x',function(){
+   $(this).closest('tr').remove();
+});
+
+let dataCart = [];
+
+$('#btn-add-cart').click(function(){
+let productName = $(this).closest('.modal-content').find('.col-sm-8').find('h2').text(); // dò để phân biệt tên từng ản phẩm
+let quantity = $(this).closest('.modal-content').find('.col-sm-8').find('.input-qty').val(); // so luong dat hang
+let price = 50000;
+let maProduct ;
+let objProduct = {
+    "Name":productName,
+    "Quantity":quantity,
+    "Price":price,
+    "ID": maProduct
+}
+dataCart.push(objProduct);
+let lengthCart = $("#tbody-cart-cfthegioi").find('tr').length;
+let strTrBody = "";
+strTrBody += "<tr>";
+strTrBody += '<td class="td-cart ">'+(lengthCart + 1)+'</td>';
+strTrBody += '<td class="td-cart cart-items">'+(objProduct.Name)+'</td>';
+strTrBody += '<td class="td-cart">'+(objProduct.Price)+'</td>';
+strTrBody += '<td class="td-cart">'+(objProduct.Quantity)+'</td>';
+strTrBody += '<td class="td-cart " ><i class="fa fa-close btn-x"></i></td>';
+strTrBody += "</tr>";
+$("#tbody-cart-cfthegioi").append(strTrBody); // de them strTrBody vao trong bang san pham
+totalPrice();
+ // push vao array 
+});
+
+function totalPrice(){
+    let total = 0;
+    $.each(dataCart,function(i, el){
+        total += (el.Quantity * el.Price);
+    })
+    $('#total-cart').text('Tong tien:           ' + total);
+
+}
