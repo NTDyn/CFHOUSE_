fd1be97a089075c47792793db6ec149c3e2ab358
Lê Đức Toàn <anhlai789@gmail.com>
2022-11-27 11:51:33 +0700

    commit

--- assets/js/dungchung.js
+++ assets/js/dungchung.js
@@ -1,99 +1,99 @@
 function addHeader() {
     document.write(`
     <header> 
-    <div class="topbar">
-        <div class="container">
-            <div class="topbar-row">
-                <div class="topbar-left">
-                    <ul>
-                        <li> 
-                            <span class="welcome-text">Chào mừng bạn đến với Coffee House</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="topbar-right">
-                    <div class="f-right">
-                        <ul class="ul-account">
-                            <li>
-                                <a href="./register.html" title="Đăng ký">Đăng ký</a>
-                            </li>
-                            <li class="line">/</li>
-                            <li>
-                                <a href="./log-in.html" title="Đăng nhập">Đăng nhập</a>
+        <div class="topbar">
+            <div class="container">
+                <div class="topbar-row">
+                    <div class="topbar-left">
+                        <ul>
+                            <li> 
+                                <span class="welcome-text">Chào mừng bạn đến với Coffee House</span>
                             </li>
                         </ul>
                     </div>
+                    <div class="topbar-right">
+                        <div class="f-right">
+                            <ul class="ul-account">
+                                <li>
+                                    <a href="./register.html"  title="Đăng ký">Đăng ký</a>
+                                </li>
+                                <li class="line">/</li>
+                                <li>
+                                    <a href="./log-in.html" title="Đăng nhập">Đăng nhập</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="mid-header">
-        <div class="container">
-            <div class="mid-header-row">
-                <div class="header-nav">
-                    <ul class="nav-left">
-                        <li class="nav-item ">
-                            <a class="a-img active" href="/">
-                                Trang chủ
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="a-img " href="">
-                                Giới thiệu
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="a-img " href="">
-                                Sản phẩm
-                                <i class="fa-solid fa-caret-down"></i>
-                            </a>
-                            <ul class="subnav">
-                                <li class="subnav-item"><a href="">Cà phê thế giới</a></li>
-                                <li class="subnav-item"><a href="">Cà phê pha Việt</a></li>
-                                <li class="subnav-item"><a href="">Cà phê cảm hứng</a></li>
-                                <li class="subnav-item"><a href="">Đồ uống</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul class="logo_center">
-                        <li class="logo">
-                            <a href="/" class="logo-wrapper">
-                                <img src="./assets/img/logo/logo-img.webp" alt="" class="logo-img">
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="nav-right">
-                        <li class="nav-item">
-                            <a class="a-img " href="">
-                                Dịch vụ
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="a-img " href="">
-                                Tin tức
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="a-img " href="">
-                                Liên hệ
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="cart f-right">
-                        <div class="cart-contain">
-                            <a href="" style="text-decoration: none;">
-                                <div class="visible-index">
-                                    <i class="fa-solid fa-bag-shopping active" style="font-size: 25px;"></i>
-                                    <span class="count-item">0</span>
-                                </div>
-                            </a>
+        <div class="mid-header">
+            <div class="container">
+                <div class="mid-header-row">
+                    <div class="header-nav">
+                        <ul class="nav-left">
+                            <li class="nav-item ">
+                                <a class="a-img active" href="/">
+                                    Trang chủ
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="a-img " href="">
+                                    Giới thiệu
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="a-img " href="">
+                                    Sản phẩm
+                                    <i class="fa-solid fa-caret-down"></i>
+                                </a>
+                                <ul class="subnav">
+                                    <li class="subnav-item"><a href="">Cà phê thế giới</a></li>
+                                    <li class="subnav-item"><a href="">Cà phê pha Việt</a></li>
+                                    <li class="subnav-item"><a href="">Cà phê cảm hứng</a></li>
+                                    <li class="subnav-item"><a href="">Đồ uống</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <ul class="logo_center">
+                            <li class="logo">
+                                <a href="/" class="logo-wrapper">
+                                    <img src="./assets/img/logo/logo-img.webp" alt="" class="logo-img">
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="nav-right">
+                            <li class="nav-item">
+                                <a class="a-img " href="">
+                                    Dịch vụ
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="a-img " href="">
+                                    Tin tức
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="a-img " href="">
+                                    Liên hệ
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="cart f-right">
+                            <div class="cart-contain">
+                                <a href="" style="text-decoration: none;">
+                                    <div class="visible-index">
+                                        <i class="fa-solid fa-bag-shopping active" style="font-size: 25px;"></i>
+                                        <span class="count-item">0</span>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                </d>
+                    </d>
+                </div>
             </div>
         </div>
-    </div>
-</header>    
+    </header>  
     `)
 }
 
@@ -168,4 +168,119 @@ function addFooter() {
         </div>
     </div>
     `)
+}
+
+function User(name,password, email, phone) {
+	this.name = name || '';
+	this.email = email || '';
+	this.password = password || '';
+	this.phone = phone || '';
+}
+
+function getCurrentUser() {
+    return JSON.parse(localStorage.getItem('CurrentUser'));
+}
+function setCurrentUser(u) {
+    localStorage.setItem('CurrentUser', JSON.stringify(u));
+}
+
+function getListUser() {
+    var data = JSON.parse(localStorage.getItem('ListUser')) || []
+    var l = [];
+    for (var d of data) {
+        l.push(d);
+    }
+    return l;
+}
+
+function setListUser(l) {
+    localStorage.setItem('ListUser', JSON.stringify(l));
+}
+
+function updateListUser(u, newData) {
+    var list = getListUser();
+    for (var i = 0; i < list.length; i++) {
+        if (equalUser(u, list[i])) {
+            list[i] = (newData ? newData : u);
+        }
+    }
+    setListUser(list);
+}
+
+function equalUser(u1, u2) {
+	return (u1.username == u2.username && u1.pass == u2.pass);
+}
+
+function logIn(e) {
+    // event.preventDefault();
+    var name = document.querySelector("#name").value;
+    var password = document.querySelector("#password").value;
+    // var newUser = new User(name, password);
+    // var listUser = getListUser();
+    // for(var u of listUser) {
+    //     if(u.name == newUser.name && u.password == newUser.password) {
+    //         alert("Đăng nhập thành công");
+    //         location.href = "index.html";
+    //     } else if(u.name != newUser.name && u.password == newUser.password || u.name == newUser.name && u.password != newUser.password) {
+    //         alert("username hoặc password sai!!");
+    //     } else {
+    //         alert("Tài khoản chưa được đăng kí!!");
+    //     }
+    // }
+    for (var u of listUser) {
+        if (equalUser(newUser, u)) {
+            alert("Đăng nhập thành công!!");
+            setCurrentUser(u);
+
+            // Reload lại trang -> sau khi reload sẽ cập nhật luôn giỏ hàng khi hàm setupEventTaiKhoan chạy
+            location.reload();
+            return false;
+        }
+    }
+   
+}
+
+
+
+function signUp(e) {
+    event.preventDefault();
+    var topBarRight = document.querySelector(".topbar-right")
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#password").value;
+    var phone = document.querySelector("#phone").value;
+    var newUser = new User(name, password, email, phone);
+    // var user = {
+    //     username : name,
+    //     email : email,
+    //     password : password,
+    //     phone : phone
+    // }
+
+    // Lấy dữ liệu các khách hàng hiện có
+    var listUser = getListUser();
+    
+    // Kiểm tra xem dữ liệu form có trùng với khách hàng đã có không
+    for (var u of listUser) {
+        if (newUser.name == u.name) {
+            alert('Tên đăng nhập đã có người sử dụng !!');
+            return false;
+        }
+    }
+    
+    // // Lưu người mới vào localstorage
+    listUser.push(newUser);
+    localStorage.setItem('ListUser', JSON.stringify(listUser));
+    
+    // // Đăng nhập vào tài khoản mới tạo
+    localStorage.setItem('CurrentUser', JSON.stringify(newUser));
+    // alert('Đăng kí thành công, Bạn sẽ được tự động đăng nhập!');
+    alert('Đăng kí thành công, Bạn sẽ được tự động đăng nhập!');
+    window.location.href = "index.html";
+    return false;
+}
+
+function logOut() {
+    localStorage.removeItem('CurrentUser');
+    location.reload();
 }
