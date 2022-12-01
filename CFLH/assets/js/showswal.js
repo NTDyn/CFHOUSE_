0aed6f20dcf564dca3bc8431d8750040da9a186f
NTDyn <114789770+NTDyn@users.noreply.github.com>
2022-12-01 16:28:52 +0700

    update

--- CFLH/assets/js/showswal.js
+++ CFLH/assets/js/showswal.js
@@ -1,7 +1,24 @@
+let usernameValue = $('#userNameEle').val();
+let phoneValue = $('#userPhoneEle').val();
+let contentValue = $('#userContentEle').val();
+
 $('#btn-send-lh').click(function(){
-    Swal.fire({
-        text: 'ĐÃ GỬI LIÊN HỆ THÀNH CÔNG',
-        icon: 'success',
-        timer: 1700
-      })
+    if(usernameValue == '' || phoneValue =='' || contentValue =='')
+    {
+        Swal.fire({
+            text: 'VUI LÒNG ĐIỀN ĐẦY ĐỦ THÔNG TIN',
+            icon: 'error',
+            timer: 1700
+        })
+    }
+
+    else
+    {
+        Swal.fire({
+            text: 'ĐÃ GỬI THÀNH CÔNG',
+            icon: 'success',
+            timer: 1700
+        })
+    }
 })
+
